57df15fa0eb5c22d61e22302369ae19895fbbe86
Jordan Christensen <jordan.c4922@gmail.com>
2019-10-30 01:55:16 +0800

    Add recording functionality

--- SimpleAudioRecorder/SimpleAudioRecorder/Recorder.swift
+++ SimpleAudioRecorder/SimpleAudioRecorder/Recorder.swift
@@ -7,3 +7,55 @@
 //
 
 import AVFoundation
+
+protocol RecorderDelegate {
+    func recorderDidChangeState(_ recorder: Recorder)
+}
+
+class Recorder: NSObject {
+    
+    var audioRecorder: AVAudioRecorder?
+    
+    var isRecording: Bool {
+        audioRecorder?.isRecording ?? false
+    }
+    
+    override init() {
+        super.init()
+        
+        
+    }
+    
+    func record() {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: [.withInternetDateTime])
+        
+        // <date>.caf
+        let filename = documentsDirectory
+                      .appendingPathComponent(name)
+                      .appendingPathExtension("caf")
+        
+        // 44.1 kHz
+        let format = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 1)!
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: filename, format: format)
+        } catch {
+            NSLog("Recording error: \(error)")
+        }
+        
+        audioRecorder?.record()
+    }
+    
+    func stop() {
+        audioRecorder?.stop()
+    }
+    
+    func toggle() {
+        if isRecording {
+            stop()
+        } else {
+            record()
+        }
+    }
+}
